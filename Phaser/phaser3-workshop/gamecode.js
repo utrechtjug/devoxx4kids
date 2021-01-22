a1f72dc23d2821759b18c4fd6b6058afccc88f93
EWLameijer <eric_wubbo@hotmail.com>
2021-01-22 06:59:34 +0100

    JavaScript-code eruitgehaald en directory verder wat opgeruimd

---
+++ Phaser/phaser3-workshop/gamecode.js
@@ -0,0 +1,40 @@
+var instellingen = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+
+    },
+    scene: {
+        preload: spelLaden,
+        create: spelOpbouwen,
+        update: spelUpdaten
+    }
+};
+
+let speler;
+let sterren;
+let bommen;
+let terreinGroep;
+let cursors;
+let score = 0;
+let gameOver = false;
+let scoreTekst;
+
+let game = new Phaser.Game(instellingen);
+
+function spelLaden() {
+    this.load.image('achtergrond', 'afbeeldingen/achtergrond.png');
+    this.load.image('platform', 'afbeeldingen/platform.png');
+    this.load.image('ster', 'afbeeldingen/ster.png');
+    this.load.image('bom', 'afbeeldingen/bom.png');
+    this.load.spritesheet('speler', 'afbeeldingen/speler.png', { frameWidth: 32, frameHeight: 48 });
+}
+
+function spelOpbouwen() {
+
+}
+
+function spelUpdaten() {
+
+}
